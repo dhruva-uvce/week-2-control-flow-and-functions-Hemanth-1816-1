2107a53a69d5ed03990466618bb21b9656001dc8
Hemanth-1816 <hemu63594@gmail.com>
2026-04-09 23:25:35 +0530

    Update Q01.py

--- Q01.py
+++ Q01.py
@@ -14,3 +14,20 @@
 # Sample Output:  Grade: B
 
 # --- YOUR CODE HERE ---
+i=int(input("enter the score:"))
+if i<0 or i>100:
+  print("invalid score")
+elif i>=90 and i<=100:
+  print("a")
+elif i>=80 and i<=89:
+  print("b")
+elif i>=70 and i<=79:
+  print("c")
+elif i>=60 and i<=69:
+  print("D")
+elif i<0 and i>100:
+  print("invalid score")
+else :
+   print("f")
+  
+  
